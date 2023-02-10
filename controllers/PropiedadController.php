df046c611b947d643c0ba45efe325db0ac1feb4c
branmeco <branmeco17@gmail.com>
2023-02-10 13:12:49 -0500

    Actualizando en la base de datos

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -80,7 +80,38 @@ class PropiedadController
 
         $errores = Propiedad::getErrores();
 
-        $router -> render('/propiedades/actualizar', [
+        //Método post para actualizar
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            // Asignar los atributos
+            $args = $_POST['propiedad'];
+
+            $propiedad->sincronizar($args);
+
+
+            // Validación
+            $errores = $propiedad->validar();
+
+            // Subida de archivos
+            // Generar un nombre único
+            $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
+
+            if ($_FILES['propiedad']['tmp_name']['imagen']) {
+                $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
+                $propiedad->setImagen($nombreImagen);
+            }
+
+            if (empty($errores)) {
+                // Almacenar la imagen
+                if ($_FILES['propiedad']['tmp_name']['imagen']) {
+                    $image->save(CARPETA_IMAGENES . $nombreImagen);
+                }
+
+                $propiedad->guardar();
+            }
+        }
+
+        $router->render('/propiedades/actualizar', [
             'propiedad' => $propiedad,
             'errores' => $errores,
             'vendedores' => $vendedores
