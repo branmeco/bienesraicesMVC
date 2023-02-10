5ac0c89ddb0014666aa245f2cbc06f40abc29ac6
branmeco <branmeco17@gmail.com>
2023-02-10 18:44:32 -0500

    Creando el controlador para vendedores

---
+++ controllers/VendedorController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Controllers;
+
+class VendedorController
+{
+    public static function crear()
+    {
+        echo "Crear Vendedor";
+    }
+
+    public static function actualizar()
+    {
+        echo "Actualizar vendedor";
+    }
+    public static function eliminar()
+    {
+        echo "Eliminar vendedor";
+    }
+}
