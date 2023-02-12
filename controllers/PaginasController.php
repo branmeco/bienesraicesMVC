94288e4bac8a73862d892b09f2e435e26d1347f2
branmeco <branmeco17@gmail.com>
2023-02-11 20:02:35 -0500

    Creando la página de nosotros

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -11,13 +11,14 @@ class PaginasController
         $propiedades = Propiedad::get(3);
         $inicio = true;
 
-        $router->render('paginas/index',[
+        $router->render('paginas/index', [
             'propiedades' => $propiedades,
             'inicio' => $inicio
         ]);
     }
-    public static function nosotros()
+    public static function nosotros(Router $router)
     {
+        $router->render('paginas/nosotros', []);
     }
     public static function propiedades()
     {
